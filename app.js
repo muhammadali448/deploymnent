6ee69f6bb2af55bc73505b427b2ee6129a7b89ff
Ali448 <33634855+Ali448@users.noreply.github.com>
2018-02-02 00:50:36 +0500

    version 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
-
+const port = process.env.PORT || 5000;
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
@@ -13,6 +13,6 @@ app.get('/about', (req, res) => {
 });
 
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log(`Server started on port`);
 });
